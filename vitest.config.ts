367fc6426948f2285fb471b4df26bf953a64e6b5
emon <liming20110711@163.com>
2026-05-30 17:07:07 +0800

    chore: vitest 配置覆盖率 (istanbul)

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
+    include: ["test/**/*.test.ts"],
+    coverage: {
+      provider: "istanbul",
+      include: ["src/**/*.ts"],
+      exclude: ["src/cli/index.ts"],
+    },
   },
 });
